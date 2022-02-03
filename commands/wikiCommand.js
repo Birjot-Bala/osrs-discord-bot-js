409ad5847e5c22b755665481fa281676c18a080d
Birjot Bala <birjot.bala@gmail.com>
2022-02-03 16:37:46 -0500

    Got rid of src folder

--- commands/wikiCommand.js
+++ commands/wikiCommand.js
@@ -1,5 +1,5 @@
 const { SlashCommand, CommandOptionType } = require('slash-create');
-import { WIKI_URL } from '../constants'
+import { WIKI_URL } from './constants'
 
 module.exports = class WikiCommand extends SlashCommand {
   constructor(creator) {
