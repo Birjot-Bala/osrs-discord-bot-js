195360f9bd669951fa1edda93f892d088eeb993d
Birjot Bala <birjot.bala@gmail.com>
2022-02-11 13:47:18 -0500

    Added new command to find item ID

---
+++ src/commands/itemIDCommand.js
@@ -0,0 +1,29 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+const { searchForID } = require('../utils/itemSearch');
+
+module.exports = class HelloCommand extends SlashCommand {
+  constructor(creator) {
+    super(creator, {
+      name: 'itemid',
+      description: 'Return the item ID for the item',
+      options: [{
+        type: CommandOptionType.STRING,
+        required: true,
+        name: 'item',
+        description: 'What item to search the ID for?'
+      }],
+      guildIDs: ['656445531396112394']
+    });
+
+    this.filePath = __filename;
+  }
+
+  async run(ctx) {
+    const matches = searchForID(ctx.options.item);
+    if (matches.length) {
+      return `Here are the item IDs I found for '${ctx.options.item}':\n${matches.map(([itemName, itemID]) => `${itemName.padEnd(30," ")}${itemID}`).join('\n')}`;
+    } else {
+      return `Hmm... I couldn't find any item IDs for '${ctx.options.item}`
+    }
+  }
+}
---
+++ src/utils/itemSearch.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const tradeableItems = JSON.parse(fs.readFileSync('./data/tradeable_items.json'));
+
+const searchForID = (userItem, num = 3) => {
+  const re =/[ \'\,\-\(\)]/g
+  const cleanUp = (str) => str.toLowerCase().replace(re,  '');
+  const cleanUserItem = cleanUp(userItem);
+  console.log(cleanUserItem);
+
+  const results = [];
+
+  for (const [itemName, itemID] of Object.entries(tradeableItems)) {
+    if (cleanUp(itemName).includes(cleanUserItem)) {
+      if (results.push([itemName, itemID]) === num) {
+        break;
+      }
+    }
+  }
+
+  return results;
+}
+
+module.exports = {
+  searchForID
+}
