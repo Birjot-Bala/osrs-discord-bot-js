03c05a57008e24011e7757b1e9b9a9bceeff53b2
Birjot Bala <birjot.bala@gmail.com>
2022-02-13 17:51:49 -0500

    Added url for prices api

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,10 @@ const USER_AGENT = 'osrs-discord-bot-js (https://github.com/Birjot-Bala/osrs-dis
 
 // URL list
 const WIKI_URL = 'https://oldschool.runescape.wiki'
+const PRICES_URL = 'https://prices.runescape.wiki'
 
 module.exports = {
   WIKI_URL,
+  PRICES_URL,
   USER_AGENT
 }
