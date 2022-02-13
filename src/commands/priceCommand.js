6551a79a6d95f7a0dddc990e51fe0f584178f78a
Birjot Bala <birjot.bala@gmail.com>
2022-02-13 18:06:27 -0500

    Added price command

---
+++ src/commands/priceCommand.js
@@ -0,0 +1,44 @@
+const { SlashCommand, CommandOptionType } = require('slash-create');
+const { searchByID } = require('../utils/itemSearch');
+const { PRICES_URL, USER_AGENT } = require('../constants');
+
+module.exports = class HelloCommand extends SlashCommand {
+  constructor(creator) {
+    super(creator, {
+      name: 'price',
+      description: 'Retrieves the latest price for an item.',
+      options: [{
+        type: CommandOptionType.STRING,
+        name: 'item',
+        description: 'What is the item ID?'
+      }],
+      guildIDs: ['656445531396112394']
+    });
+
+    this.filePath = __filename;
+  }
+
+  async run(ctx) {
+
+    const userItemName = searchByID(ctx.options.item);
+    
+    const pricesClient = axios.create({
+      baseURL: PRICES_URL,
+      timeout: 2000,
+      headers: {'User-Agent': USER_AGENT}
+    });
+
+    return prices.get('/api/v1/osrs/latest', {
+      params: {
+        id: ctx.options.item
+      }
+    })
+    .then(resp => {
+      console.log(`${resp.status} ${resp.statusText} ${resp.config.baseURL}${resp.config.url}\n${JSON.stringify(resp.config.headers)}\n${JSON.stringify(resp.config.params)}`);
+      return `Here are the latest prices for '${userItemName[0]}':\n\`\`\`${'High'.padEnd(10,'.')}${resp.data.data[id]['high']}\n${'Low'.padEnd(10, '.')}${resp.data.data[id]['low']}\`\`\``    })
+    .catch(err => {
+      console.error(err);
+      return 'Oh no... looks like something went wrong with the /price command.'
+    });
+  }
+}
