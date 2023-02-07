ab917c018b090421eb65e58d1644c5c5e416fd59
Birjot Bala <61438495+Birjot-Bala@users.noreply.github.com>
2023-02-06 23:09:37 -0500

    resolve promises for records

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,16 +1,17 @@
 const { updateTrackerMessage } = require("./helpers");
 
 exports.handler = async function(event, context) {
+    const promises = [];
     event.Records.forEach(record => {
         const { messageAttributes: { CommandType } , body } = record;
         console.log(CommandType.stringValue, body);
 
         switch (CommandType.stringValue) {
-            case 'tracker':
-                updateTrackerMessage(JSON.parse(body));
+            case 'tracker': 
+                promises.push(updateTrackerMessage(JSON.parse(body)));
                 break;
             default: 
         }
     });
-    return {};
+    return Promise.all(promises);
   }
